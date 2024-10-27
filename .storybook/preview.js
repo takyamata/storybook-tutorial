a6f2291260d56841fd8638174c6d0fb3528e5d91
DESKTOP-4USMOFH\takuto <uvubantyo@gmail.com>
2024-10-27 17:47:01 +0900

    エラーが治らないのでshin-codeから完成したディレクトリ丸々コピー

---
+++ .storybook/preview.js
@@ -0,0 +1,20 @@
+import "../src/index.css";
+import { initialize, mswDecorator } from "msw-storybook-addon";
+
+initialize();
+
+/** @type { import('@storybook/react').Preview } */
+const preview = {
+  decorators: [mswDecorator],
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+};
+
+export default preview;
---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./index.css";
+import { Provider } from "react-redux";
+import InboxScreen from "./components/InboxScreen";
+import store from "./lib/store";
+
+//npx chromatic --project-token=chpt_988ba981abeeb08 --storybook-build-dir=storybook-static
+//aa
+
+function App() {
+	return (
+		<Provider store={store}>
+			<InboxScreen />
+		</Provider>
+	);
+}
+
+export default App;
